28614970be4ece1d8e9df674e5f9a15a328ad4da
keroken <kenichi.shinagawa@gmail.com>
2021-09-21 23:45:54 +0900

    base project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import votes from './reducers';
 
 function App() {
+  const store = createStore(votes, composeWithDevTools());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
